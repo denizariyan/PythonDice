d1089bbd7e8afc6074f14b0700033157363e3b7f
denizariyan <denizariyan@gmail.com>
2018-02-24 02:46:38 +0100

    Added loop for ease of use

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import random
 try:
     dice = random.randint(1, 6)
-    userInput = int(input("Make your guess for a 6 int dice: "))
-    if dice == userInput:
-        print("You are right! The dice was ", dice)
-    elif dice != userInput:
-        print("Not lucky today? The dice was ", dice)
-    pass
+    endFlag = 1
+    while endFlag:
+        userInput = int(input("Make your guess for a 6 int dice: "))
+        if dice == userInput:
+            print("You are right! The dice was ", dice)
+            endFlag = 0
+        elif dice != userInput:
+                    if dice < userInput:
+                            print("Thats a bit high I see. Try again")
+                    else:
+                            print("Thats a bit too low. Try again")
+                    pass
+        pass
 except Exception as e:
     print(e)
